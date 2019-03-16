9c2bba726f82c48e3e7b5162c5cdf661e77a4395
NetDwarf <netdwarf@users.noreply.github.com>
2019-03-16 13:17:12 +0100

    Add PerformanceTests
    Added tests for builtins that only act as reference
    Added memory tests that check that memory consumption doesn't exceed a
    certain amount

---
+++ IntegrationTests/PerformanceTests/Memory_BuiltIns.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace DOL.PerformanceTests.Memory
+{
+	[TestFixture]
+	class BuiltIns
+	{
+		[Test]
+		public void Primitive_object_12Bytes()
+		{
+			long before = GC.GetTotalMemory(true);
+			var obj = new object();
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("Object uses " + memoryConsumption + " bytes");
+		}
+
+		[Test]
+		public void Array_Empty_12Bytes()
+		{
+			long before = GC.GetTotalMemory(true);
+			byte[] array = { };
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("Empty Array uses " + memoryConsumption + " bytes");
+		}
+
+		[Test]
+		public void ArrayOfInt_WithTenElements_52Bytes()
+		{
+			long before = GC.GetTotalMemory(true);
+			int[] array = new int[10];
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("int array uses " + memoryConsumption + " bytes");
+		}
+
+		[Test]
+		public void ListOfInt_Init_36Bytes()
+		{
+			long before = GC.GetTotalMemory(true);
+			var list = new List<int>();
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("Initialized List of Ints uses " + memoryConsumption + " bytes");
+		}
+
+		[Test]
+		public void ListOfInt_OneIntAdded_40Bytes()
+		{
+			long before = GC.GetTotalMemory(true);
+			var list = new List<int>();
+			list.Add(1);
+			list.TrimExcess();
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("List with one Int uses " + memoryConsumption + " bytes");
+		}
+
+		[Test]
+		public void ListOfInt_TenIntAdded_76Bytes()
+		{
+			long before = GC.GetTotalMemory(true);
+			var list = new List<int>();
+			for(int i=0; i < 10; i++) { list.Add(1); }
+			list.TrimExcess();
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("List with 10 Integers uses " + memoryConsumption + " bytes");
+		}
+
+		[Test]
+		public void ListOfObjects_10ObjectsAdded_196Bytes()
+		{
+			long before = GC.GetTotalMemory(true);
+			var list = new List<object>();
+			for (int i = 0; i < 10; i++) { list.Add(new object()); }
+			list.TrimExcess();
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			//36BytesInit + 10 * ( 4BytesReference + 12BytesObject )
+			Console.WriteLine("List with 10 Objects uses " + memoryConsumption + " bytes");
+		}
+	}
+}
---
+++ IntegrationTests/PerformanceTests/Memory_GameServer.cs
@@ -0,0 +1,102 @@
+﻿using NUnit.Framework;
+using System;
+using DOL.GS;
+using DOL.UnitTests.GameServer;
+
+namespace DOL.PerformanceTests.Memory
+{
+	[TestFixture]
+	class GameServer
+	{
+		[Test]
+		public void GamePlayer_100Times_Init_LessThanOneMegabyte()
+		{
+			long before = GC.GetTotalMemory(true);
+			GamePlayer[] players = new GamePlayer[100];
+			for (int i = 0; i < players.Length; i++)
+			{
+				players[i] = GamePlayer.CreateTestableGamePlayer();
+			}
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("100 GamePlayer take " + memoryConsumption + " bytes");
+			Assert.Less(memoryConsumption, 10 * 1000 * 1000);
+		}
+
+		[Test]
+		public void GamePlayer_100Times_WithEightBaseStatsSet_LessThanTenMegabyte()
+		{
+			long before = GC.GetTotalMemory(true);
+			GamePlayer[] players = new GamePlayer[100];
+			for (int i = 0; i < players.Length; i++)
+			{
+				players[i] = GamePlayer.CreateTestableGamePlayer();
+				for (int j = 1; j <= 8; j++)
+				{
+					players[i].Boni.Add(Bonus.Base.ComponentOf((eProperty)j).Create(60));
+				}
+			}
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("100 GamePlayer with 8 base properties set consume " + memoryConsumption + " bytes");
+			Assert.Less(memoryConsumption, 10 * 1000 * 1000);
+		}
+
+		[Test]
+		public void GamePlayer_100Times_WithHundredItemPropertiesSet_LessThanTenMegabyte()
+		{
+			long before = GC.GetTotalMemory(true);
+			GamePlayer[] players = new GamePlayer[100];
+			for (int i = 0; i < players.Length; i++)
+			{
+				players[i] = GamePlayer.CreateTestableGamePlayer();
+				for (int j = 1; j <= 100; j++)
+				{
+					players[i].Boni.Add(Bonus.Item.ComponentOf((eProperty)j).Create(60));
+				}
+			}
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("100 GamePlayer with 100 item properties set consume " + memoryConsumption + " bytes");
+			Assert.Less(memoryConsumption, 10 * 1000 * 1000);
+		}
+
+		[Test]
+		public void GameNPC_With8BasePropertiesSet_LessThan100Megabyte()
+		{
+			long before = GC.GetTotalMemory(true);
+			GameNPC[] npcs = new GameNPC[10000];
+			for (int i = 0; i < npcs.Length; i++)
+			{
+				npcs[i] = Create.FakeNPC();
+				for (int j = 1; j <= 8; j++)
+				{
+					npcs[i].Boni.Add(Bonus.Base.ComponentOf((eProperty)j).Create(60));
+				}
+			}
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("10000 GameNPC with 8 base properties set consume " + memoryConsumption + " bytes");
+			Assert.Less(memoryConsumption, 100 * 1000 * 1000);
+		}
+
+		[Test]
+		public void Boni_10000Times_With8BasePropertiesSet_LessThan20MegaByte()
+		{
+			long before = GC.GetTotalMemory(true);
+			Boni[] boniArray = new Boni[10000];
+			for (int i = 0; i < boniArray.Length; i++)
+			{
+				boniArray[i] = new Boni(null);
+				for (int j = 1; j <= 8; j++)
+				{
+					boniArray[i].Add(Bonus.Base.ComponentOf((eProperty)j).Create(60));
+				}
+			}
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("10000 Boni with 8 base properties set consume " + memoryConsumption + " bytes");
+			Assert.Less(memoryConsumption, 20 * 1000 * 1000);
+		}
+	}
+}
