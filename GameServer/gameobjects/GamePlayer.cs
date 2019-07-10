ef731e435c680a775599e9d854d90a29609be31c
NetDwarf <netdwarf@users.noreply.github.com>
2019-07-10 10:55:00 +0200

    Remove test constructor from GamePlayer

--- GameServer/gameobjects/GamePlayer.cs
+++ GameServer/gameobjects/GamePlayer.cs
@@ -15606,15 +15606,6 @@ namespace DOL.GS
 				.ToString();
 		}
 
-        public static GamePlayer CreateTestableGamePlayer() { return CreateTestableGamePlayer(new DefaultCharacterClass()); }
-
-        public static GamePlayer CreateTestableGamePlayer(ICharacterClass charClass) { return new GamePlayer(charClass); }
-
-        private GamePlayer(ICharacterClass charClass) : base()
-        {
-            m_characterClass = charClass;
-        }
-
 		/// <summary>
 		/// Creates a new player
 		/// </summary>
