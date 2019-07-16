5ad41ba8492e195babdc8464f4089f00170ee3ec
NetDwarf <netdwarf@users.noreply.github.com>
2019-07-16 12:32:51 +0200

    Add getter/setter for BonusAggregator
    Create parameter object BonusComponent for: eProperty, eBonusSource
    Create parameter object Bonus for: BonusComponent, int (bonusValue)
    Add BonusAggregator.GetValueOf(BonusComponent)
    Add BonusAggregator.Set(Bonus)

--- GameServer/gameobjects/Boni/BonusAggregator.cs
+++ GameServer/gameobjects/Boni/BonusAggregator.cs
@@ -13,6 +13,16 @@ namespace DOL.GS
 		private IPropertyIndexer DebuffCategory { get; } = new PropertyIndexer();
 		private IPropertyIndexer SpecDebuffCategory { get; } = new PropertyIndexer();
 
+		public int GetValueOf(BonusComponent component)
+		{
+			return GetIndexerFor(component.Source.ID)[component.Type.ID];
+		}
+
+		public void Set(Bonus bonus)
+		{
+			GetIndexerFor(bonus.Source.ID)[bonus.Type.ID] = bonus.Value;
+		}
+
 		public void ClearItemBonuses()
 		{
 			ItemBonus = new PropertyIndexer();
@@ -31,6 +41,75 @@ namespace DOL.GS
 				case eBonusSource.SpecDebuff: return SpecBuffBonusCategory;
 				default: throw new ArgumentException();
 			}
+		}
+	}
+
+	public class Bonus
+	{
+		public BonusType Type => Component.Type;
+		public BonusSource Source => Component.Source;
+		public BonusComponent Component { get; }
+		public int Value { get; }
+
+		public Bonus(BonusComponent component, int value)
+		{
+			Component = component;
+			Value = value;
+		}
+
+		public static BonusSource Ability => new BonusSource(eBonusSource.Ability);
+		public static BonusSource Item => new BonusSource(eBonusSource.Item);
+		public static BonusSource BaseBuff => new BonusSource(eBonusSource.BaseBuff);
+		public static BonusSource SpecBuff => new BonusSource(eBonusSource.SpecBuff);
+		public static BonusSource ExtraBuff => new BonusSource(eBonusSource.ExtraBuff);
+		public static BonusSource Debuff => new BonusSource(eBonusSource.Debuff);
+		public static BonusSource SpecDebuff => new BonusSource(eBonusSource.SpecDebuff);
+
+		public static BonusType Constitution => new BonusType(eProperty.Constitution);
+	}
+
+	public class BonusComponent
+	{
+		public BonusType Type { get; }
+		public BonusSource Source { get; }
+
+		public BonusComponent(BonusType type, BonusSource source)
+		{
+			Type = type;
+			Source = source;
+		}
+
+		public Bonus Create(int value)
+		{
+			return new Bonus(this, value);
+		}
+	}
+
+	public class BonusType
+	{
+		public eProperty ID { get; }
+
+		public BonusType(eProperty typeID)
+		{
+			ID = typeID;
+		}
+
+		public BonusComponent Ability => new BonusComponent(this, Bonus.Ability);
+		public BonusComponent Item => new BonusComponent(this, Bonus.Item);
+		public BonusComponent BaseBuff => new BonusComponent(this, Bonus.BaseBuff);
+		public BonusComponent SpecBuff => new BonusComponent(this, Bonus.SpecBuff);
+		public BonusComponent ExtraBuff => new BonusComponent(this, Bonus.ExtraBuff);
+		public BonusComponent Debuff => new BonusComponent(this, Bonus.Debuff);
+		public BonusComponent SpecDebuff => new BonusComponent(this, Bonus.SpecDebuff);
+	}
+
+	public class BonusSource
+	{
+		public eBonusSource ID { get; }
+
+		public BonusSource(eBonusSource sourceID)
+		{
+			ID = sourceID;
 		}
 	}
 
---
+++ UnitTests/UT_BonusAggregator.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using DOL.GS;
+
+namespace DOL.UnitTests.GameServer
+{
+	[TestFixture]
+	class UT_BonusAggregator
+	{
+		[Test]
+		public void GetValueOf_ConstitutionAbility_Init_Zero()
+		{
+			var bonusAggregator = createBonusAggregator();
+			var constitution = eProperty.Constitution;
+			var ability = eBonusSource.Ability;
+
+			int actual = bonusAggregator.GetValueOf(Bonus.Constitution.Ability);
+			int expected = 0;
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GetValueOf_ConstitutionAbility_AbilityIndexerSetToOneForConstitution_One()
+		{
+			var bonusAggregator = createBonusAggregator();
+			var constitution = eProperty.Constitution;
+			var ability = eBonusSource.Ability;
+
+			bonusAggregator.GetIndexerFor(ability)[constitution] = 1;
+
+			int actual = bonusAggregator.GetValueOf(Bonus.Constitution.Ability);
+			int expected = 1;
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GetValueOf_ConstitutionItem_ItemIndexerSetToOneForConstitution_One()
+		{
+			var bonusAggregator = createBonusAggregator();
+			var constitution = eProperty.Constitution;
+			var item = eBonusSource.Item;
+
+			bonusAggregator.GetIndexerFor(item)[constitution] = 1;
+
+			int actual = bonusAggregator.GetValueOf(Bonus.Constitution.Item);
+			int expected = 1;
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void Set_ConstitutionItem2_GetValueOfConstitutionItemIs2()
+		{
+			var bonusAggregator = createBonusAggregator();
+			var constitution = eProperty.Constitution;
+			var item = eBonusSource.Item;
+			
+			bonusAggregator.Set(Bonus.Constitution.Item.Create(2));
+
+			int actual = bonusAggregator.GetValueOf(Bonus.Constitution.Item);
+			int expected = 2;
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void Set_OneAbilityConstitution_GetValueOfAbilityConstitutionIsOne()
+		{
+			var bonusAggregator = createBonusAggregator();
+			var constitution = eProperty.Constitution;
+			var ability = eBonusSource.Ability;
+			var bonus = Bonus.Constitution.Ability.Create(1);
+
+			bonusAggregator.Set(bonus);
+
+			int actual = bonusAggregator.GetValueOf(Bonus.Constitution.Ability);
+			int expected = 1;
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GetValueOf_ConstitutionAbility_AbilityIndexerOfConstitutionSetToOne_One()
+		{
+			var bonusAggregator = createBonusAggregator();
+			var bonus = Bonus.Constitution.Ability.Create(1);
+
+			bonusAggregator.GetIndexerFor(bonus.Source.ID)[bonus.Type.ID] = bonus.Value;
+
+			int actual = bonusAggregator.GetValueOf(bonus.Component);
+			int expected = 1;
+			Assert.AreEqual(expected, actual);
+		}
+
+		private BonusAggregator createBonusAggregator() => new BonusAggregator();
+	}
+}
