92caff78646cf9fba665bb1fe80ed0fa55d713fa
NetDwarf <netdwarf@users.noreply.github.com>
2019-07-18 18:35:23 +0200

    Add memory tests for Bonus and BonusAggregator

--- IntegrationTests/PerformanceTests/Memory_GameServer.cs
+++ IntegrationTests/PerformanceTests/Memory_GameServer.cs
@@ -83,6 +83,62 @@ namespace DOL.PerformanceTests.Memory
 			Assert.Less(memoryConsumption, 100 * 1000 * 1000);
 		}
 
+		[Test]
+		public void BonusAggregator_10000Times_With8AbilityPropertiesSet_LessThan20MegaByte()
+		{
+			long before = GC.GetTotalMemory(true);
+			BonusAggregator[] boniArray = new BonusAggregator[10000];
+			for (int i = 0; i < boniArray.Length; i++)
+			{
+				boniArray[i] = new BonusAggregator();
+				for (int j = 1; j <= 8; j++)
+				{
+					var bonusType = new BonusType((eProperty)j);
+					boniArray[i].Set(bonusType.Ability.WithValue(60));
+				}
+			}
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("10000 Boni with 8 base properties set consume " + memoryConsumption + " bytes");
+			Assert.Less(memoryConsumption, 20 * 1000 * 1000);
+		}
+
+		[Test]
+		public void BonusAggregator_100Times_WithHundredItemPropertiesSet_LessThan2Megabyte()
+		{
+			long before = GC.GetTotalMemory(true);
+			BonusAggregator[] boniArray = new BonusAggregator[100];
+			for (int i = 0; i < boniArray.Length; i++)
+			{
+				boniArray[i] = new BonusAggregator();
+				for (int j = 0; j < 100; j++)
+				{
+					var newBonusType = new BonusType((eProperty)j);
+					boniArray[i].Set(newBonusType.Item.WithValue(60));
+				}
+			}
+
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("100 Boni with 100 item properties set consume " + memoryConsumption + " bytes");
+			Assert.Less(memoryConsumption, 2 * 1000 * 1000);
+		}
+
+		[Test]
+		public void Bonus_1000Times_LessThan80kb()
+		{
+			long before = GC.GetTotalMemory(true);
+			Bonus[] boniArray = new Bonus[1000];
+			for (int i = 0; i < boniArray.Length; i++)
+			{
+				boniArray[i] = Bonus.Constitution.Ability.WithValue(5);
+			}
+			long after = GC.GetTotalMemory(true);
+			long memoryConsumption = after - before;
+			Console.WriteLine("1000 Bonus(es) consume " + memoryConsumption + " bytes");
+			Assert.Less(memoryConsumption, 80 * 1000);
+		}
+
 		private GamePlayer createPlayer()
 		{
 			return Create.FakePlayer();
